f2b38048b5c3db0bb0556df0137b7ced4c175284
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-17 12:14:51 +0100

    feat: snack 1

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+const namesString = names.toString()
+console.log(namesString)
